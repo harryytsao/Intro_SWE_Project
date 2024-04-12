6d87eb56b78f7fab9d38a891a01165eff0ef3678
kellyyuan333 <kellyyuan333@gmail.com>
2024-04-12 13:36:30 -0400

    update frontend

--- zenstate/app/timer/page.tsx
+++
@@ -1,9 +0,0 @@
-import Navbar from '@/components/Navbar'
-import Stopwatch from '@/components/Stopwatch'
-export default function Login() {
-    return <>
-        <Navbar />
-        <h1>Timer</h1>
-        <Stopwatch/>
-    </>
-}
