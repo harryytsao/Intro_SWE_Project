4e5d8933270c00117be54a893de9609ede96c1b7
harry <harrytsao12345@gmail.com>
2024-04-16 14:09:10 -0400

    change clerk to dark theme

--- zenstate/app/(root)/layout.tsx
+++ zenstate/app/(root)/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-
+import { dark } from "@clerk/themes";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,7 +15,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
       <html lang="en">
         <body className={'${inter.className} bg-slate-800 text-slate-100'}>
           {children}
