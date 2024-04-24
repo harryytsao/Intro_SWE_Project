277f1e0fe3ba2a773d4484075cb1ecbec01a603c
XJXuan888 <115206157+XJXuan888@users.noreply.github.com>
2024-04-24 15:36:23 -0400

    Add comments to page.tsx

--- zenstate/app/(root)/home/page.tsx
+++ zenstate/app/(root)/home/page.tsx
@@ -10,7 +10,7 @@ import PointTracker from '@/components/Points/points';
 import SearchFriends from '@/components/SearchFriends/SearchFriends';
 
 export default function Page() {
-
+  // State variables for point tracker value and added friend count
   const [pointTrackerValue, setPointTrackerValue] = useState(0); 
   const [addedFriend, setAddedFriend]= useState(0)
 
@@ -18,22 +18,27 @@ export default function Page() {
     <div>
       <NavbarUser />
       <BackgroundGradientAnimation>
+        {/* Stopwatch component to track time */}
         <div style={{ position: 'absolute', top: '8%', left: '5%', zIndex: 999 }}>
           <Stopwatch setPointTrackerValue={setPointTrackerValue} />
         </div>
 
+         {/* Form for weekly points */}
         <div style={{ position: 'absolute', bottom: '8%', left: '5%', zIndex: 999 }}>
           <WeekForm points={pointTrackerValue} />
         </div>
 
+        {/* Leaderboard component */}
         <div style={{ position: 'absolute', bottom: '30%', right: '10%', zIndex: 999 }}>
           <Leaderboard addedPoint={pointTrackerValue} addedFriend={addedFriend} />
         </div>
 
+        {/* Point tracker component */}
         <div style={{ position: 'absolute', bottom: '25%', right: '35%', zIndex: 999 }}> 
           <PointTracker time={pointTrackerValue} />
         </div>
 
+        {/* Search friends component */}
         <div style={{ position: 'absolute', top: '10%', right: '10%', zIndex: 999 }}>
           <SearchFriends  cb={setAddedFriend} curr={addedFriend}/>
         </div>
